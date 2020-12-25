ef1e7cdb914eb15036d0d09f498dcdb1b3c9654f
Faten <riahigaten05@gmail.com>
2020-12-26 00:28:58 +0100

    first commit

---
+++ src/test/java/com/project/serviceconfig/ServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.serviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
